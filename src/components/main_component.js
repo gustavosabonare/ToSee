45b355bc8c9a58145fe0907cf6e55cf38172a51a
Gustavo <gustavosabonare@hotmail.com>
2017-03-07 18:42:12 -0300

    adding react-bootstrap

--- src/components/main_component.js
+++ src/components/main_component.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
+import {Nav, NavItem} from 'react-bootstrap';
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Bem Vindo</h1>
-        <ul >
-          <li><Link to='/search'>Search Movies</Link></li>
-          <li><Link to='/list'>Movies List</Link></li>
-        </ul>
+        <h1>Welcome</h1>
+          <Link to="/">Home</Link>
+          <Link to="/search">Search Movies</Link>
+          <Link to="/list">Movies List</Link>
       </div>
     );
   }
